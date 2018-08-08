f7a5bec883a55d5b109c1f0286efa4926b5f8ec7
Morten Poul Jensen <mpj@signifly.com>
2018-08-08 15:49:39 +0200

    Remove reject as it has no hasMacro method

--- src/BuilderMacroServiceProvider.php
+++ src/BuilderMacroServiceProvider.php
@@ -4,7 +4,6 @@ namespace Signifly\BuilderMacros;
 
 use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Database\Eloquent\Builder;
 
 class BuilderMacroServiceProvider extends ServiceProvider
 {
@@ -13,9 +12,6 @@ class BuilderMacroServiceProvider extends ServiceProvider
         Collection::make(glob(__DIR__.'/macros/*.php'))
             ->mapWithKeys(function ($path) {
                 return [$path => pathinfo($path, PATHINFO_FILENAME)];
-            })
-            ->reject(function ($macro) {
-                return Builder::hasMacro($macro);
             })
             ->each(function ($macro, $path) {
                 require_once $path;
